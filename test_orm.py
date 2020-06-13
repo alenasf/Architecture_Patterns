b08ef4b805462915ef315551fb0d63623cf6c962
alenasf <30708007+alenasf@users.noreply.github.com>
2020-06-13 00:32:58 -0700

    test_orm.py

--- test_orm.py
+++ test_orm.py
@@ -89,8 +89,30 @@ def test_retrieving_allocations(session):
     }
 
 
+# Using SQLAlchemy directly in our API endpoint
+''' 
+@flask.route.gubbins
+def allocate_endpoint():
+    session = start_session()
+    
+    # extract order line from request
+    line = OrderLine(
+        request.json['orderid'],
+        request.json['sku'], 
+        request.json['qty'],
+    )
+    
+    # load all batches from the DB
+    batches = session.query(Batch).all()
+    
+    # call our domain service
+    allocate(line, batches)
+    
+    # save the allocation back to the database
+    session.commit()
+    # return 201
 
-
+'''
 
 
 
